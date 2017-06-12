4131470b91ffb228d85adab28970233bef0fce1d
Maarten de Boer <maarten@cloudstek.nl>
2017-06-13 01:12:36 +0200

    Make bin/*.js executable

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('./gulp')(['babel', 'eslint']);
+const chmod = require('gulp-chmod');
 
 gulp.task('default', ['build']);
 
-gulp.task('build', ['babel', 'eslint']);
+gulp.task('build', ['babel', 'eslint'], () => {
+    return gulp.src('./bin/*.js')
+        .pipe(chmod(0o755))
+        .pipe(gulp.dest('./bin'));
+});
 
 gulp.task('watch', ['babel', 'eslint', 'build'], done => {
     gulp.watch(['./**/*.js.flow', '!node_modules', '!node_modules/**'], ['babel']);
